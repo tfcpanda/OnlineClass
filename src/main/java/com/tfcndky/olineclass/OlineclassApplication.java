1b3d70a8df4f16df9401613c046a1dc362988b62
TFC <3100228060@qq.com>
2021-03-10 19:32:02 +0800

    第一次上交

---
+++ src/main/java/com/tfcndky/olineclass/OlineclassApplication.java
@@ -0,0 +1,13 @@
+package com.tfcndky.olineclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlineclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlineclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfcndky/olineclass/OlineclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfcndky.olineclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlineclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
