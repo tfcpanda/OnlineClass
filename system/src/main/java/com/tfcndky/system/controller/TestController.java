a1672bb0baaf86cd7eb0d9dd1b3bb4f91cd638a9
TFC <3100228060@qq.com>
2021-03-10 21:05:43 +0800

    1.在gate增加system路由转发配置，调试成功

--- system/src/main/java/com/tfcndky/system/controller/TestController.java
+++ system/src/main/java/com/tfcndky/system/controller/TestController.java
@@ -3,6 +3,8 @@ package com.tfcndky.system.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class TestController {
 
