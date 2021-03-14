8935be6ea76542efcaf9f59ddedee6fd093cb427
TFC <3100228060@qq.com>
2021-03-15 00:10:02 +0800

    1.business显示成功 在controller和service层注意大小写，大小写错误编译成功但是找不到页面，输出不了数据。

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -7,3 +7,12 @@ create table `test` (
 
 insert into `test` (id, name) values (1, '测试');
 insert into `test` (id, name) values (2, '测试2');
+
+-- 大章
+drop table if exists `chapter`;
+create table `chapter` (
+                           `id` char(8) not null comment 'id',
+                           `course_id` char(8) comment '课程id',
+                           `name` varchar(50) comment '名称',
+                           primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='大章';
---
+++ server/src/main/java/com/course/server/mapper/ChapterMapper.java
@@ -0,0 +1,31 @@
+package com.course.server.mapper;
+
+import com.course.server.domain.Chapter;
+import com.course.server.domain.ChapterExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ChapterMapper {
+    long countByExample(ChapterExample example);
+
+    int deleteByExample(ChapterExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(Chapter record);
+
+    int insertSelective(Chapter record);
+
+    List<Chapter> selectByExample(ChapterExample example);
+
+    Chapter selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") Chapter record, @Param("example") ChapterExample example);
+
+    int updateByExample(@Param("record") Chapter record, @Param("example") ChapterExample example);
+
+    int updateByPrimaryKeySelective(Chapter record);
+
+    int updateByPrimaryKey(Chapter record);
+}
