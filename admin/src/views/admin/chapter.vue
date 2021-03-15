f90772ccec6f2f0579044cf574a521e4e67b6096
TFC <3100228060@qq.com>
2021-03-16 00:38:53 +0800

    1.大章页面真实显示

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -4,45 +4,21 @@
   <table id="sample-table-1" class="table table-striped table-bordered table-hover">
     <thead>
     <tr>
-      <th class="center">
-        <label class="position-relative">
-          <input type="checkbox" class="ace"/>
-          <span class="lbl"></span>
-        </label>
-      </th>
-      <th>Domain</th>
-      <th>Price</th>
-      <th class="hidden-480">Clicks</th>
-
-      <th>
-        <i class="ace-icon fa fa-clock-o bigger-110 hidden-480"></i>
-        Update
-      </th>
-      <th class="hidden-480">Status</th>
-
-      <th></th>
+      <th>ID</th>
+      <th>名称</th>
+      <th>课程ID</th>
+      <th>操作</th>
     </tr>
     </thead>
 
     <tbody>
-    <tr>
-      <td class="center">
-        <label class="position-relative">
-          <input type="checkbox" class="ace"/>
-          <span class="lbl"></span>
-        </label>
-      </td>
+    <tr v-for="chapter in chapters">
+      <td>{{chapter.id}}</td>
+      <td>{{chapter.name}}</td>
+      <td>{{chapter.courseId}}</td>
 
-      <td>
-        <a href="#">ace.com</a>
-      </td>
-      <td>$45</td>
-      <td class="hidden-480">3,330</td>
-      <td>Feb 12</td>
 
-      <td class="hidden-480">
-        <span class="label label-sm label-warning">Expiring</span>
-      </td>
+
 
       <td>
         <div class="hidden-sm hidden-xs btn-group">
@@ -100,245 +76,11 @@
     </tr>
 
     <tr>
-      <td class="center">
-        <label class="position-relative">
-          <input type="checkbox" class="ace"/>
-          <span class="lbl"></span>
-        </label>
-      </td>
+      <td> 123</td>
 
-      <td>
-        <a href="#">base.com</a>
-      </td>
-      <td>$35</td>
-      <td class="hidden-480">2,595</td>
-      <td>Feb 18</td>
 
-      <td class="hidden-480">
-        <span class="label label-sm label-success">Registered</span>
-      </td>
-
-      <td>
-        <div class="hidden-sm hidden-xs btn-group">
-          <button class="btn btn-xs btn-success">
-            <i class="ace-icon fa fa-check bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-info">
-            <i class="ace-icon fa fa-pencil bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-danger">
-            <i class="ace-icon fa fa-trash-o bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-warning">
-            <i class="ace-icon fa fa-flag bigger-120"></i>
-          </button>
-        </div>
-
-        <div class="hidden-md hidden-lg">
-          <div class="inline position-relative">
-            <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-              <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-            </button>
-
-            <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-              <li>
-                <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-                                <span class="blue">
-                                  <i class="ace-icon fa fa-search-plus bigger-120"></i>
-                                </span>
-                </a>
-              </li>
-
-              <li>
-                <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-                                <span class="green">
-                                  <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
-                                </span>
-                </a>
-              </li>
-
-              <li>
-                <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-                                <span class="red">
-                                  <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                                </span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </td>
-    </tr>
-
-    <tr>
-      <td class="center">
-        <label class="position-relative">
-          <input type="checkbox" class="ace"/>
-          <span class="lbl"></span>
-        </label>
-      </td>
-
-      <td>
-        <a href="#">max.com</a>
-      </td>
-      <td>$60</td>
-      <td class="hidden-480">4,400</td>
-      <td>Mar 11</td>
-
-      <td class="hidden-480">
-        <span class="label label-sm label-warning">Expiring</span>
-      </td>
-
-      <td>
-        <div class="hidden-sm hidden-xs btn-group">
-          <button class="btn btn-xs btn-success">
-            <i class="ace-icon fa fa-check bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-info">
-            <i class="ace-icon fa fa-pencil bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-danger">
-            <i class="ace-icon fa fa-trash-o bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-warning">
-            <i class="ace-icon fa fa-flag bigger-120"></i>
-          </button>
-        </div>
-
-        <div class="hidden-md hidden-lg">
-          <div class="inline position-relative">
-            <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-              <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-            </button>
-
-            <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-              <li>
-                <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-                                <span class="blue">
-                                  <i class="ace-icon fa fa-search-plus bigger-120"></i>
-                                </span>
-                </a>
-              </li>
-
-              <li>
-                <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-                                <span class="green">
-                                  <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
-                                </span>
-                </a>
-              </li>
-
-              <li>
-                <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-                                <span class="red">
-                                  <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                                </span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </td>
-    </tr>
-
-    <tr>
-      <td class="center">
-        <label class="position-relative">
-          <input type="checkbox" class="ace"/>
-          <span class="lbl"></span>
-        </label>
-      </td>
-
-      <td>
-        <a href="#">best.com</a>
-      </td>
-      <td>$75</td>
-      <td class="hidden-480">6,500</td>
-      <td>Apr 03</td>
-
-      <td class="hidden-480">
-        <span class="label label-sm label-inverse arrowed-in">Flagged</span>
-      </td>
-
-      <td>
-        <div class="hidden-sm hidden-xs btn-group">
-          <button class="btn btn-xs btn-success">
-            <i class="ace-icon fa fa-check bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-info">
-            <i class="ace-icon fa fa-pencil bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-danger">
-            <i class="ace-icon fa fa-trash-o bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-warning">
-            <i class="ace-icon fa fa-flag bigger-120"></i>
-          </button>
-        </div>
-
-        <div class="hidden-md hidden-lg">
-          <div class="inline position-relative">
-            <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-              <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-            </button>
-
-            <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-              <li>
-                <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-                                <span class="blue">
-                                  <i class="ace-icon fa fa-search-plus bigger-120"></i>
-                                </span>
-                </a>
-              </li>
-
-              <li>
-                <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-                                <span class="green">
-                                  <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
-                                </span>
-                </a>
-              </li>
-
-              <li>
-                <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-                                <span class="red">
-                                  <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                                </span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </td>
-    </tr>
-
-    <tr>
-      <td class="center">
-        <label class="position-relative">
-          <input type="checkbox" class="ace"/>
-          <span class="lbl"></span>
-        </label>
-      </td>
-
-      <td>
-        <a href="#">pro.com</a>
-      </td>
-      <td>$55</td>
-      <td class="hidden-480">4,250</td>
-      <td>Jan 21</td>
-
-      <td class="hidden-480">
-        <span class="label label-sm label-success">Registered</span>
-      </td>
+      <td>123</td>
+      <td>123</td>
 
       <td>
         <div class="hidden-sm hidden-xs btn-group">
@@ -401,7 +143,12 @@
 <script>
 export default {
   name: "chapter",
-  mounted: function() {
+  data:function (){
+    return{
+      chapters:[]
+    }
+  },
+  mounted: function () {
     let _this = this;
     _this.list();
     //激活样式之一
@@ -409,10 +156,11 @@ export default {
 
   },
   methods: {
-    list(){
+    list() {
       let _this = this;
-      _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then((response)=>{
-        console.log("查询大章节列表结果：",response);
+      _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then((response) => {
+        console.log("查询大章节列表结果：", response);
+        _this.chapters =response.data;
       })
     }
   }
