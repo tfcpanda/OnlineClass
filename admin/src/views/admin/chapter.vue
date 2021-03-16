da7b1abc45b6d1bd2522826705a6bdcfe31a6660
TFC <3100228060@qq.com>
2021-03-16 14:51:39 +0800

    1.集成了sweetalert2,删除时弹出确认狂。

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -185,14 +185,33 @@ export default {
     },
     del(id) {
       let _this = this;
-        _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response) => {
-        console.log("删除大章节列表结果：", response);
-        let resp = response.data;
-        if (resp.success) {
+      Swal.fire({
+        title: '确认删除?',
+        text: "删除后不可恢复!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response) => {
+            console.log("删除大章节列表结果：", response);
+            let resp = response.data;
+            if (resp.success) {
 
-          _this.list(1);
+              _this.list(1);
+              Swal.fire(
+                  '删除成功!',
+                  '删除成功',
+                  'success'
+              )
+            }
+          })
+
         }
       })
+
     },
 
   }
