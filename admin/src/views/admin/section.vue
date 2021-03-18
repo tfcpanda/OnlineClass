05aeff5a1e6cb3db9255746cc9bb8573465ca792
TFC <3100228060@qq.com>
2021-03-18 14:32:47 +0800

    1.vue生成器为组件名称增加模块作为前缀

--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -131,7 +131,7 @@
     import Pagination from "../../components/pagination";
     export default {
         components: {Pagination},
-        name: "section",
+        name: "business-section",
         data: function() {
             return {
                 section: {},
