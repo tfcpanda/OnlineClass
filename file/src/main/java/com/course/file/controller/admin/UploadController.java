90c82801ead4d8e4150ac207d7a69e5b9535ede6
TFC <3100228060@qq.com>
2021-03-21 00:02:15 +0800

    1.文件上传功能开发，文件上传实时显示与保存

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -40,6 +40,8 @@ public class UploadController {
         LOG.info(dest.getAbsolutePath());
 
         ResponseDto responseDto = new ResponseDto();
+        responseDto.setContent("http://127.0.0.1:9000/file/f/teacher/" + key + "-" + fileName);
+
         return responseDto;
     }
 
