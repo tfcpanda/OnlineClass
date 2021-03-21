25d19a8796c65c85a2c540eccef27cbcaaf0375d
TFC <3100228060@qq.com>
2021-03-22 00:51:15 +0800

    1.根据文件标识来修改或新增文件记录，修改分片索引从1开始

--- server/src/main/java/com/course/server/service/FileService.java
+++ server/src/main/java/com/course/server/service/FileService.java
@@ -10,7 +10,7 @@ import com.course.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.util.Date;
@@ -50,11 +50,13 @@ public class FileService {
      */
     public void save(FileDto fileDto) {
         File file = CopyUtil.copy(fileDto, File.class);
-        if (StringUtils.isEmpty(fileDto.getId())) {
+        File fileDb = selectByKey(fileDto.getKey());
+        if (fileDb == null) {
             System.out.println("insert增加");
             this.insert(file);
         } else {
-            this.update(file);
+            fileDb.setShardIndex(fileDto.getShardIndex());
+            this.update(fileDb);
         }
 
     }
@@ -95,5 +97,15 @@ public class FileService {
 
     }
 
+    public File selectByKey(String key) {
+        FileExample example = new FileExample();
+        example.createCriteria().andKeyEqualTo(key);
+        List<File> fileList = fileMapper.selectByExample(example);
+        if (CollectionUtils.isEmpty(fileList)) {
+            return null;
+        } else {
+            return fileList.get(0);
+        }
+    }
 
 }
