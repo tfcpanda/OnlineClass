df9034973c1f7829c98937267ec3b5db5d496f4a
TFC <3100228060@qq.com>
2021-03-22 11:27:25 +0800

    1.文件上传:增加上传进度条组件

--- admin/public/static/js/loading.js
+++ admin/public/static/js/loading.js
@@ -3,7 +3,6 @@ Loading = {
         $.blockUI({
             message: '<img src="/static/image/loading.gif" />',
             css: {
-                zIndex: "10011",
                 padding: "10px",
                 left: "50%",
                 width: "80px",
