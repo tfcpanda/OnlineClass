3daf0171f82d74f2028f52c4929d9bc849e18471
TFC <3100228060@qq.com>
2021-03-23 00:25:41 +0800

    1.用户管理和登录：集成显示图形验证码kaptcha

--- server/src/main/java/com/course/server/config/CorsConfig.java
+++ server/src/main/java/com/course/server/config/CorsConfig.java
@@ -1,27 +1,27 @@
-//package com.course.server.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-///**
-// * @author 田付成
-// * @date 2021/3/15 23:39
-// *
-// *
-// * 前后端分离的跨域问题。
-// */
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedHeaders(CorsConfiguration.ALL)
-//                .allowedMethods(CorsConfiguration.ALL)
-//                .allowCredentials(true)
-//                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
-//    }
-//
-//}
+package com.course.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author 田付成
+ * @date 2021/3/15 23:39
+ *
+ *
+ * 前后端分离的跨域问题。
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
+    }
+
+}
