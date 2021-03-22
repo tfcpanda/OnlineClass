3ca14d047f9f6aeeeb6e7ddfd89af4653641b7ae
TFC <3100228060@qq.com>
2021-03-22 20:34:46 +0800

    1.用户管理与登录：增加用户名是否存在校验 2.增加自定义业务异常

---
+++ server/src/main/java/com/course/server/ecxeption/BusinessExceptionCode.java
@@ -0,0 +1,24 @@
+package com.course.server.ecxeption;
+
+/**
+ * @author 田付成
+ * @date 2021/3/22 20:05
+ */
+public enum BusinessExceptionCode {
+    USER_LOGIN_NAME_EXIST("登录名已存在"),
+            ;
+
+    private String desc;
+
+    BusinessExceptionCode(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
--- server/src/main/java/com/course/server/service/UserService.java
+++ server/src/main/java/com/course/server/service/UserService.java
@@ -2,14 +2,17 @@ package com.course.server.service;
 
 import com.course.server.domain.User;
 import com.course.server.domain.UserExample;
-import com.course.server.dto.UserDto;
 import com.course.server.dto.PageDto;
+import com.course.server.dto.UserDto;
+import com.course.server.ecxeption.BusinessException;
+import com.course.server.ecxeption.BusinessExceptionCode;
 import com.course.server.mapper.UserMapper;
 import com.course.server.util.CopyUtil;
 import com.course.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
@@ -62,9 +65,11 @@ public class UserService {
      * @param user
      */
     private void insert(User user) {
-
         user.setId(UuidUtil.getShortUuid());
-
+        User userDb = this.selectByLoginName(user.getLoginName());
+        if (userDb != null) {
+            throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+        }
         userMapper.insert(user);
 
     }
@@ -86,6 +91,22 @@ public class UserService {
     public void delete(String id) {
         userMapper.deleteByPrimaryKey(id);
 
+    }
+
+    /**
+     * 根据登录名查询用户信息
+     * @param loginName
+     * @return
+     */
+    public User selectByLoginName(String loginName) {
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andLoginNameEqualTo(loginName);
+        List<User> userList = userMapper.selectByExample(userExample);
+        if (CollectionUtils.isEmpty(userList)) {
+            return null;
+        } else {
+            return userList.get(0);
+        }
     }
 
 
