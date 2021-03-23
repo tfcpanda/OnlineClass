d0e2dd5066dff747fed9bf209d50fa2b8284652c
TFC <3100228060@qq.com>
2021-03-23 23:56:39 +0800

    1.通用权限管理:模态框【保存】按钮时，保存角色用户关联表

--- server/src/main/java/com/course/server/dto/RoleDto.java
+++ server/src/main/java/com/course/server/dto/RoleDto.java
@@ -23,6 +23,16 @@ public class RoleDto {
 
     private List<String> resourceIds;
 
+    private List<String> userIds;
+
+    public List<String> getUserIds() {
+        return userIds;
+    }
+
+    public void setUserIds(List<String> userIds) {
+        this.userIds = userIds;
+    }
+
     public List<String> getResourceIds() {
         return resourceIds;
     }
@@ -63,6 +73,7 @@ public class RoleDto {
         sb.append(", name='").append(name).append('\'');
         sb.append(", desc='").append(desc).append('\'');
         sb.append(", resourceIds=").append(resourceIds);
+        sb.append(", userIds=").append(userIds);
         sb.append('}');
         return sb.toString();
     }
