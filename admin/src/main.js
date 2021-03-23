cdc8a9845a45e7a152453851b5dbd982617f8228
TFC <3100228060@qq.com>
2021-03-23 20:04:38 +0800

    在header头加上token

--- admin/src/main.js
+++ admin/src/main.js
@@ -15,39 +15,46 @@ axios.defaults.withCredentials = true;
  * axios拦截器
  */
 axios.interceptors.request.use(function (config) {
-  console.log("请求：", config);
-  return config;
-}, error => {});
+    console.log("请求：", config);
+    let token = Tool.getLoginUser().id;
+    if (Tool.isNotEmpty(token)) {
+        config.headers.token = token;
+        console.log("请求headers增加token:", token);
+    }
+    return config;
+}, error => {
+});
 axios.interceptors.response.use(function (response) {
-  console.log("返回结果：", response);
-  return response;
-}, error => {});
+    console.log("返回结果：", response);
+    return response;
+}, error => {
+});
 
 // 全局过滤器
 Object.keys(filter).forEach(key => {
-  Vue.filter(key, filter[key])
+    Vue.filter(key, filter[key])
 });
 
 // 路由登录拦截
 router.beforeEach((to, from, next) => {
-  // 要不要对meta.loginRequire属性做监控拦截
-  if (to.matched.some(function (item) {
-    return item.meta.loginRequire
-  })) {
-    let loginUser = Tool.getLoginUser();
-    if (Tool.isEmpty(loginUser)) {
-      next('/login');
+    // 要不要对meta.loginRequire属性做监控拦截
+    if (to.matched.some(function (item) {
+        return item.meta.loginRequire
+    })) {
+        let loginUser = Tool.getLoginUser();
+        if (Tool.isEmpty(loginUser)) {
+            next('/login');
+        } else {
+            next();
+        }
     } else {
-      next();
+        next();
     }
-  } else {
-    next();
-  }
 });
 
 new Vue({
-  router,
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
 
-console.log("环境",process.env.NODE_ENV);
+console.log("环境", process.env.NODE_ENV);
