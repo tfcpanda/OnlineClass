e286ef182e83a288dab2e0761ea5752cd48d2106
TFC <3100228060@qq.com>
2021-03-23 23:02:18 +0800

    1.通用权限管理:资源数的显示

--- system/src/main/java/com/course/system/controller/admin/ResourceController.java
+++ system/src/main/java/com/course/system/controller/admin/ResourceController.java
@@ -1,6 +1,7 @@
 package com.course.system.controller.admin;
 
 import com.course.server.dto.PageDto;
+import com.course.server.dto.ResourceDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.service.ResourceService;
 import com.course.server.util.ValidatorUtil;
@@ -9,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/resource")
@@ -62,6 +64,17 @@ public class ResourceController {
         LOG.info("id:{}", id);
         resourceService.delete(id);
 
+        return responseDto;
+    }
+
+    /**
+     * 资源树查询
+     */
+    @GetMapping("/load-tree")
+    public ResponseDto loadTree() {
+        ResponseDto responseDto = new ResponseDto();
+        List<ResourceDto> resourceDtoList = resourceService.loadTree();
+        responseDto.setContent(resourceDtoList);
         return responseDto;
     }
 }
