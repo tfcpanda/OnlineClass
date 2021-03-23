8310d138a3a977bbb395b915f7c7217adde325c2
TFC <3100228060@qq.com>
2021-03-23 19:55:55 +0800

    解决跨域sessionId变化，导致验证失败。

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -46,7 +46,7 @@
                         <label class="block clearfix">
                           <span class="block input-icon input-icon-right">
                             <div class="input-group">
-                              <input type="text" class="form-control" placeholder="验证码">
+                              <input v-model="user.imageCode" type="text" class="form-control" placeholder="验证码">
                               <span class="input-group-addon" id="basic-addon2">
                                 <img v-on:click="loadImageCode()" id="image-code" alt="验证码"/>
                               </span>
@@ -114,8 +114,8 @@ export default {
   data: function () {
     return {
       user: {},
-      remember: true // 默认勾选记住我
-
+      remember: true, // 默认勾选记住我
+      imageCodeToken: ""
     }
   },
   mounted: function () {
@@ -154,6 +154,9 @@ export default {
         _this.user.password = hex_md5(_this.user.password + KEY);
       }
 
+      _this.user.imageCodeToken = _this.imageCodeToken;
+
+
       Loading.show();
       _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/login', _this.user).then((response)=>{
         Loading.hide();
@@ -182,7 +185,9 @@ export default {
 
           _this.$router.push("/welcome")
         } else {
-          Toast.warning(resp.message)
+          Toast.warning(resp.message);
+          _this.user.password = "";
+          _this.loadImageCode();
         }
       });
 
