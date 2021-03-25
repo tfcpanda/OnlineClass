a9d63352edeecd5a139ab968675602d5f7b9b22f
TFC <3100228060@qq.com>
2021-03-25 15:07:00 +0800

    1.前端界面设计:添加分页功能

--- web/src/views/list.vue
+++ web/src/views/list.vue
@@ -4,6 +4,13 @@
       <div class="album py-5 bg-light">
         <div class="container">
           <div class="row">
+            <div class="col-md-12">
+              <pagination ref="pagination" v-bind:list="listCourse"></pagination>
+            </div>
+
+          </div>
+          <br>
+          <div class="row">
             <div v-for="o in courses" class="col-md-4">
               <the-course v-bind:course="o"></the-course>
             </div>
@@ -19,10 +26,11 @@
 <script>
 
 import TheCourse from "../components/the-course";
+import Pagination from "../components/pagination";
 
 export default {
   name: 'index',
-  components: {TheCourse},
+  components: {Pagination, TheCourse},
   data: function () {
     return {
       courses: [],
@@ -30,6 +38,7 @@ export default {
   },
   mounted() {
     let _this = this;
+    _this.$refs.pagination.size = 5;
     _this.listCourse(1);
   },
   methods: {
@@ -40,11 +49,13 @@ export default {
       let _this = this;
       _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/web/course/list', {
         page: page,
-        size: 3,
+        size: _this.$refs.pagination.size,
       }).then((response) => {
         let resp = response.data;
         if (resp.success) {
           _this.courses = resp.content.list;
+          _this.$refs.pagination.render(page, resp.content.total);
+
         }
       }).catch((response) => {
         console.log("error：", response);
