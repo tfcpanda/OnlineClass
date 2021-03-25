4b5d982276917426b27d9ee04c1eac93fd704c4b
TFC <3100228060@qq.com>
2021-03-25 14:20:18 +0800

    1.前端界面设计:增加条状样式

---
+++ web/src/views/list.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <main role="main">
+
+      <section class="jumbotron text-center">
+        <div class="container">
+          <h1>在线客车</h1>
+          <p class="lead text-muted">在线教育平台的编写</p>
+
+        </div>
+      </section>
+
+      <div class="album py-5 bg-light">
+        <div class="container">
+          <h1>课程还未上线</h1>
+        </div>
+      </div>
+
+    </main>
+  </div>
+
+</template>
+
+<script>
+
+import TheCourse from "../components/the-course";
+
+export default {
+  name: 'index',
+  components: {TheCourse},
+  data: function () {
+    return {
+      news: [],
+    }
+  },
+  mounted() {
+    let _this = this;
+    _this.listNew();
+  },
+  methods: {
+    /**
+     * 查询新上好课
+     */
+    listNew() {
+      let _this = this;
+      _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/web/course/list-new').then((response) => {
+        console.log("查询新上好课结果：", response);
+        let resp = response.data;
+        if (resp.success) {
+          _this.news = resp.content;
+        }
+      }).catch((response) => {
+        console.log("error：", response);
+      })
+    },
+  }
+}
+</script>
+
+<style>
+.title1 {
+  margin-bottom: 2rem;
+  color: #fafafa;
+  letter-spacing: 0;
+  text-shadow: 0px 1px 0px #999, 0px 2px 0px #888, 0px 3px 0px #777, 0px 4px 0px #666, 0px 5px 0px #555, 0px 6px 0px #444, 0px 7px 0px #333, 0px 8px 7px #001135;
+  font-size: 2rem;
+}
+
+.title2 {
+  margin-bottom: 2rem;
+  color: transparent;
+  -webkit-text-stroke: 1px black;
+  letter-spacing: 0.04em;
+  font-size: 2rem;
+}
+
+
+</style>
