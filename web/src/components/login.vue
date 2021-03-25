9978f6f00a78b782265cbf7b79ff033fad87e0de
TFC <3100228060@qq.com>
2021-03-26 00:17:56 +0800

    1. 会员登录开发：登录成功后会保存账号密码【BUG】登录后验证码也会被保存

--- web/src/components/login.vue
+++ web/src/components/login.vue
@@ -160,6 +160,14 @@ export default {
     //---------------登录框、注册框、忘记密码框切换-----------------
     toLoginDiv() {
       let _this = this;
+      // 从缓存中获取记住的用户名密码，如果获取不到，说明上一次没有勾选“记住我”
+      let rememberMember = LocalStorage.get(LOCAL_KEY_REMEMBER_MEMBER);
+      if (rememberMember) {
+        _this.member = rememberMember;
+      }
+
+      // 显示登录框时就刷新一次验证码图片
+      _this.loadImageCode();
       _this.MODAL_STATUS = _this.STATUS_LOGIN
     },
     toRegisterDiv() {
@@ -214,15 +222,16 @@ export default {
             // 如果勾选记住我，则将用户名密码保存到本地缓存
             // 原：这里需要保存密码明文，否则登录时又会再加一层密
             // 新：这里保存密码密文，并保存密文md5，用于检测密码是否被重新输入过
+            // 补交：这里保存密码密文，并保存密文md5，用于检测密码是否被重新输入过
+
             let md5 = hex_md5(_this.member.password);
             LocalStorage.set(LOCAL_KEY_REMEMBER_MEMBER, {
-              loginName: loginMember.loginName,
-              // password: _this.member.passwordShow,
+              mobile: loginMember.mobile,
               password: _this.member.password,
               md5: md5
             });
           } else {
-            // 没有勾选“记住我”时，要把本地缓存清空，否则按照mounted的逻辑，下次打开时会自动显示用户名密码
+            // 没有勾选“记住我”时，要把本地缓存清空，否则按照mounted的逻辑，否则下次显示登录框时会自动显示用户名密码
             LocalStorage.set(LOCAL_KEY_REMEMBER_MEMBER, null);
           }
 
