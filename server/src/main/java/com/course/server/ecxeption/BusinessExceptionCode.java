8e2594c87c76aa1a32753220770c7a03fc2887e2
TFC <3100228060@qq.com>
2021-03-26 11:34:45 +0800

    1.登录界面的设计：前后端实现重置密码

--- server/src/main/java/com/course/server/ecxeption/BusinessExceptionCode.java
+++ server/src/main/java/com/course/server/ecxeption/BusinessExceptionCode.java
@@ -5,13 +5,12 @@ package com.course.server.ecxeption;
  * @date 2021/3/22 20:05
  */
 public enum BusinessExceptionCode {
+    MEMBER_NOT_EXIST("会员不存在"),
     USER_LOGIN_NAME_EXIST("登录名已存在"),
     LOGIN_ERROR("用户名或密码不存在"),
     MOBILE_CODE_TOO_FREQUENT("短信请求过于频繁"),
-
     LOGIN_USER_ERROR("用户名不存在或密码错误"),
     LOGIN_MEMBER_ERROR("手机号不存在或密码错误"),
-
     MOBILE_CODE_ERROR("短信验证码不正确"),
     MOBILE_CODE_EXPIRED("短信验证码不存在或已过期，请重新发送短信"),
     ;
