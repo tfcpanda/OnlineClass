99bdff5f41195e953819a360f378abafe3c5bd55
TFC <3100228060@qq.com>
2021-03-26 14:00:24 +0800

    1.网关地址更改，加速访问。 2.OSS上传拼出oss地址。

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -41,6 +41,10 @@ public class UploadController {
     @Value("${file.path}")
     private String FILE_PATH;
 
+    @Value("${oss.domain}")
+    private String OSS_DOMAIN;
+
+
     @Value("${vod.accessKeyId}")
     private String accessKeyId;
 
@@ -159,7 +163,7 @@ public class UploadController {
         FileDto fileDto = fileService.findByKey(key);
         if (fileDto != null) {
             if (StringUtils.isEmpty(fileDto.getVod())) {
-                fileDto.setPath(FILE_DOMAIN + fileDto.getPath());
+                fileDto.setPath(OSS_DOMAIN + fileDto.getPath());
             } else {
                 DefaultAcsClient vodClient = VodUtil.initVodClient(accessKeyId, accessKeySecret);
                 GetMezzanineInfoResponse response = VodUtil.getMezzanineInfo(vodClient, fileDto.getVod());
